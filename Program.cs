379e7284714db4e4042baed2f4a0c04f1c47bd14
kurumuz <kurumuz1@gmail.com>
2018-04-25 23:01:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -99,27 +99,4 @@ namespace rpcs3Updater
              }
         }
 	}
-		public class Windows
-		{
-			public string datetime { get; set; }
-			public string download { get; set; }
-		}
-		public class Linux
-		{
-			public string datetime { get; set; }
-			public string download { get; set; }
-		}
-
-		public class LatestBuild
-		{
-			public string pr { get; set; }
-			public Windows windows { get; set; }
-			public Linux linux { get; set; }
-		}
-
-		public class RootObject
-		{
-			public int return_code { get; set; }
-			public LatestBuild latest_build { get; set; }
-		}
 }
